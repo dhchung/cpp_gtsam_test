cf3f17b8b64dfdfb542519626101d36d6b8cc4cf
Dongha Chung <haetae.dongha@gmail.com>
2020-10-13 17:12:35 +0900

    first commit

---
+++ build/CMakeFiles/testing_gtsam.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dongha/dev/ship_hull_inspection/cpp_test/gtsam_test/helloworld.cpp" "/home/dongha/dev/ship_hull_inspection/cpp_test/gtsam_test/build/CMakeFiles/testing_gtsam.dir/helloworld.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_OPTIONAL_ALLOW_BINDING_TO_RVALUES"
+  "BOOST_OPTIONAL_CONFIG_ALLOW_BINDING_TO_RVALUES"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/cmake/GTSAM/../../../include"
+  "/usr/local/include/gtsam/3rdparty/metis"
+  "/usr/local/include/gtsam/3rdparty/SuiteSparse_config"
+  "/usr/local/include/gtsam/3rdparty/CCOLAMD"
+  "/usr/local/include"
+  "/usr/local/include/gtsam/3rdparty/Eigen"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dongha/dev/ship_hull_inspection/cpp_test/gtsam_test
+# Build directory: /home/dongha/dev/ship_hull_inspection/cpp_test/gtsam_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
